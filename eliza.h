35e2181ec4b1b011bf5e33b35bd935b6fdc19e78
bka9 <keviea@gmail.com>
2011-03-10 22:45:40 +0000

    fixed a define in eliza.h

--- eliza.h
+++ eliza.h
@@ -7,32 +7,34 @@
 #include <ppapi/cpp/instance.h>
 #include <string>
 
-class Eliza{
-    public:
-        Eliza(pp::Instance* instance_);
-        ~Eliza();
-        bool Start();
-    
-    private:
-        static const int kBufferSize = 4096;
-        void OnOpen(int32_t result);
-        void OnRead(int32_t result);
-        void ReadBody();
+namespace eliza{
+    class Eliza{
+        public:
+            explicit Eliza(pp::Instance* instance_);
+            virtual ~Eliza();
+            virtual bool Start();
         
-        void ReportResult(const std::string& fname,
-            const std::string& text,
-            bool success);
-          // Calls JS reportResult() and self-destroy (aka delete this)
-        void ReportResultAndDie(const std::string& fname,
-            const std::string& text,
-            bool success);
-                
-        PP_Instance instance_id_;
-        pp::URLRequestInfo url_request_;
-        pp::URLLoader url_loader_;  // URLLoader provides an API to download URLs.
-        char buffer_[kBufferSize];  // buffer for pp::URLLoader::ReadResponseBody().
-        std::string url_response_body_;  // Contains downloaded data.
-        pp::CompletionCallbackFactory<Eliza> cc_factory_;
-};
+        private:
+            static const int kBufferSize = 4096;
+            void OnOpen(int32_t result);
+            void OnRead(int32_t result);
+            void ReadBody();
+            
+            void ReportResult(const std::string& fname,
+                const std::string& text,
+                bool success);
+              // Calls JS reportResult() and self-destroy (aka delete this)
+            void ReportResultAndDie(const std::string& fname,
+                const std::string& text,
+                bool success);
+                    
+            PP_Instance instance_id_;
+            pp::URLRequestInfo url_request_;
+            pp::URLLoader url_loader_;  // URLLoader provides an API to download URLs.
+            char buffer_[kBufferSize];  // buffer for pp::URLLoader::ReadResponseBody().
+            std::string url_response_body_;  // Contains downloaded data.
+            pp::CompletionCallbackFactory<Eliza> cc_factory_;
+    };
+}
 
 #endif
